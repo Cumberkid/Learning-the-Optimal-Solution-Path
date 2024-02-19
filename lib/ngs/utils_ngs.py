dba20caf1b8215eb30e9a1a45157bb14578ab591
Cumberkid <40275622+Cumberkid@users.noreply.github.com>
2024-02-19 12:28:27 -0800

    Update utils_ngs.py

--- lib/ngs/utils_ngs.py
+++ lib/ngs/utils_ngs.py
@@ -2,17 +2,10 @@ import torch
 from lib.ngs.log_reg_module import Logistic_Regression
 from lib.ngs.log_reg_solver import test
 from lib.ngs.fair_reg_solver import fair_test
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-)
 
 """Helper function that takes in a list of coarse grid models and returns the simulated losses and errors over $\lambda\in[0,1]$ compared to the exact solutions."""
 # return the simulated losses accross the solution path
-def get_losses(lam_min, lam_max, fine_delta_lam, intercepts, weights, reg_params, data_loader, loss_fn, obj=None):
+def get_losses(lam_min, lam_max, fine_delta_lam, intercepts, weights, reg_params, data_loader, loss_fn, obj=None, device='cpu'):
     
     losses = []
     coarse_grid = 0
@@ -36,27 +29,27 @@ def get_losses(lam_min, lam_max, fine_delta_lam, intercepts, weights, reg_params
                     model.linear.bias.data.fill_(intercepts[coarse_grid])
         # approximate solution uses the linear weight of coarse grid model to test for regression parameter of the fine grid
         if obj == "logit":
-            approx_loss = test(data_loader, model, loss_fn, lam)
+            approx_loss = test(data_loader, model, loss_fn, lam, device)
         elif obj == "fairness":
-            approx_loss = fair_test(data_loader, model, loss_fn, lam)
+            approx_loss = fair_test(data_loader, model, loss_fn, lam, device)
         losses.append(approx_loss)
         # print(lam, coarse_grid)
     return losses
 
 # return the absolute errors compared to the true loss accross the solution path
-def get_errs(lam_min, lam_max, true_loss_list, intercepts, weights, reg_params, data_loader, loss_fn, obj=None):
+def get_errs(lam_min, lam_max, true_loss_list, intercepts, weights, reg_params, data_loader, loss_fn, obj=None, device='cpu'):
     fine_delta_lam = (lam_max - lam_min)/(len(true_loss_list)-1)
     if obj is None:
         print("Please enter the objective: 'logit' or 'fairness'")
         return
-    losses = get_losses(lam_min, lam_max, fine_delta_lam, intercepts, weights, reg_params, data_loader, loss_fn, obj=obj)
+    losses = get_losses(lam_min, lam_max, fine_delta_lam, intercepts, weights, reg_params, data_loader, loss_fn, obj, device)
     errs = losses - true_loss_list
     return errs
 
 # return the supremum absolute error compared to the true loss accross the solution path
-def get_sup_error(lam_min, lam_max, true_loss_list, intercepts, weights, reg_params, data_loader, loss_fn, obj=None):
+def get_sup_error(lam_min, lam_max, true_loss_list, intercepts, weights, reg_params, data_loader, loss_fn, obj=None, device='cpu'):
     if obj is None:
         print("Please enter the objective: 'logit' or 'fairness'")
         return
-    errs = get_errs(lam_min, lam_max, true_loss_list, intercepts, weights, reg_params, data_loader, loss_fn, obj=obj)
+    errs = get_errs(lam_min, lam_max, true_loss_list, intercepts, weights, reg_params, data_loader, loss_fn, obj, device)
     return max(errs)
