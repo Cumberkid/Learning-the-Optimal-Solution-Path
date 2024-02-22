a811e4152b1a17f78f64a9d1c4b0327e10568820
Cumberkid <40275622+Cumberkid@users.noreply.github.com>
2024-02-22 09:35:35 -0800

    Update utils_ngs.py

--- lib/ngs/utils_ngs.py
+++ lib/ngs/utils_ngs.py
@@ -4,7 +4,7 @@ from lib.ngs.reg_solver import test
 
 """Helper function that takes in a list of coarse grid models and returns the simulated losses and errors over $\lambda\in[0,1]$ compared to the exact solutions."""
 # return the simulated losses accross the solution path
-def get_losses(lam_min, lam_max, fine_delta_lam, intercepts, weights, reg_params, data_loader, loss_fn, device='cpu'):
+def get_losses(lam_min, lam_max, fine_delta_lam, intercepts, weights, reg_params, data_loader, loss_fn, device="cpu"):
     
     losses = []
     coarse_grid = 0
@@ -24,19 +24,19 @@ def get_losses(lam_min, lam_max, fine_delta_lam, intercepts, weights, reg_params
                     model.linear.weight.copy_(torch.tensor(weights[coarse_grid]))
                     model.linear.bias.data.fill_(intercepts[coarse_grid])
         # approximate solution uses the linear weight of coarse grid model to test for regression parameter of the fine grid
-        approx_loss = test(data_loader, model, loss_fn, lam)
+        approx_loss = test(data_loader, model, loss_fn, lam, device)
         losses.append(approx_loss)
         # print(lam, coarse_grid)
     return losses
 
 # return the absolute errors compared to the true loss accross the solution path
-def get_errs(lam_min, lam_max, true_loss_list, intercepts, weights, reg_params, data_loader, loss_fn, device='cpu'):
+def get_errs(lam_min, lam_max, true_loss_list, intercepts, weights, reg_params, data_loader, loss_fn, device="cpu"):
     fine_delta_lam = (lam_max - lam_min)/(len(true_loss_list)-1)
     losses = get_losses(lam_min, lam_max, fine_delta_lam, intercepts, weights, reg_params, data_loader, loss_fn, device)
     errs = losses - true_loss_list
     return errs
 
 # return the supremum absolute error compared to the true loss accross the solution path
-def get_sup_error(lam_min, lam_max, true_loss_list, intercepts, weights, reg_params, data_loader, loss_fn, device='cpu'):
+def get_sup_error(lam_min, lam_max, true_loss_list, intercepts, weights, reg_params, data_loader, loss_fn, device="cpu"):
     errs = get_errs(lam_min, lam_max, true_loss_list, intercepts, weights, reg_params, data_loader, loss_fn, device)
     return max(errs)
