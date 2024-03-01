f1b3a2ef4b0400d86d6e3326fa63b22f325f58f4
Cumberkid <40275622+Cumberkid@users.noreply.github.com>
2024-02-29 19:06:06 -0800

    Update loss_fn.py

--- lib/ngs/loss_fn.py
+++ lib/ngs/loss_fn.py
@@ -17,6 +17,11 @@ def weighted_logit(lam, X, y, model):
     # Fair loss function
     loss = (1 - lam) * model.criterion(pred_major.view(-1, 1), y_major.view(-1, 1))
     loss += lam * model.criterion(pred_minor.view(-1, 1), y_minor.view(-1, 1))
+
+    return loss
+
+def reg_weighted_logit(lam, X, y, model):
+    loss = weighted_logit(lam, X, y, model)
     loss += 0.5 * model.ridge_term()
 
     return loss
