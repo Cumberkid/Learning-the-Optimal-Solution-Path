6190662d84ba5175085d743879d6debb69a618d4
Cumberkid <40275622+Cumberkid@users.noreply.github.com>
2024-03-17 20:06:47 -0700

    Update loss_fn_lsp.py

--- lib/lsp/loss_fn_lsp.py
+++ lib/lsp/loss_fn_lsp.py
@@ -35,6 +35,11 @@ def weighted_logit(lam, X, y, model, device="cpu"):
     # fair loss function
     loss = (1 - lam) * criterion(pred_major.view(-1, 1), y_major.view(-1, 1))
     loss += lam * criterion(pred_minor.view(-1, 1), y_minor.view(-1, 1))
+
+    return loss
+
+def reg_weighted_logit(lam, X, y, model, device="cpu"):
+    loss = weighted_logit(lam, X, y, model, device)
     loss += 0.5 * theta.norm(p=2) ** 2
 
     return loss
