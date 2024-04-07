fd8bc8c3dcabaaf4471c89ea62ec50a38495f692
Cumberkid <40275622+Cumberkid@users.noreply.github.com>
2024-04-06 23:15:05 -0700

    Update monomial_basis_generator.py

--- lib/lsp/monomial_basis_generator.py
+++ lib/lsp/monomial_basis_generator.py
@@ -1,6 +1,6 @@
 import torch
 
 # monomials
-def generate_monomials(lam, basis_dim, device='cpu'):
+def monomials(lam, basis_dim, device='cpu'):
     vec = torch.tensor([lam**i for i in range(basis_dim)])
     return bec.to(device)
