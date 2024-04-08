b26460c0fc9ff15d20df7d2c82fe0f6c4e763a18
Cumberkid <40275622+Cumberkid@users.noreply.github.com>
2024-04-07 22:17:20 -0700

    Update loss_fn_lsp.py

--- lib/lsp/loss_fn_lsp.py
+++ lib/lsp/loss_fn_lsp.py
@@ -39,7 +39,24 @@ def weighted_logit(lam, X, y, model, device="cpu"):
     return loss
 
 def reg_weighted_logit(lam, X, y, model, device="cpu"):
-    loss = weighted_logit(lam, X, y, model, device)
+    X_major = X[y == 1]
+    y_major = y[y == 1]
+    X_minor = X[y == 0]
+    y_minor = y[y == 0]
+
+    # compute predicted y_hat
+    theta = model(lam, device)
+    # print(theta[0])
+    pred_major = torch.mm(X_major, theta[1:].view(-1, 1))
+    pred_minor = torch.mm(X_minor, theta[1:].view(-1, 1))
+    if model.intercept:
+        const_major = torch.ones(len(X_major), 1).to(device)
+        pred_major += torch.mm(const_major, theta[0].view(-1, 1))
+        const_minor = torch.ones(len(X_minor), 1).to(device)
+        pred_minor += torch.mm(const_minor, theta[0].view(-1, 1))
+    # fair loss function
+    loss = (1 - lam) * criterion(pred_major.view(-1, 1), y_major.view(-1, 1))
+    loss += lam * criterion(pred_minor.view(-1, 1), y_minor.view(-1, 1))
     loss += 0.25 * 0.5 * theta.norm(p=2) ** 2
 
     return loss
