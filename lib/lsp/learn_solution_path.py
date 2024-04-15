0fdb744aeb6b7f669ab0e1da1e035884709b6964
Cumberkid <40275622+Cumberkid@users.noreply.github.com>
2024-04-14 18:41:08 -0700

    Update learn_solution_path.py

--- lib/lsp/learn_solution_path.py
+++ lib/lsp/learn_solution_path.py
@@ -1,48 +1,44 @@
 import torch
-from torch.optim.lr_scheduler import StepLR
 from lib.lsp.basis_tf_module import Basis_TF_SGD
 from lib.lsp.reg_solver_lsp import train_lsp
 from lib.lsp.utils_lsp import get_sup_error_lsp
 
-def learn_solution_path(input_dim, basis_dim, phi_lam, epochs, trainDataLoader, testDataLoader,
-                        loss_fn, lam_min, lam_max, true_losses, lr=1e-3, diminish=False, gamma=0.1, 
-                        dim_step=30, step_size=None, const=None, SGD=False, init_weight=None, 
+# step_size function take in 2 parameters: current iteration number, and a self-defined constant 
+# step_size function returns the learning rate for current iteration
+def learn_solution_path(input_dim, basis_dim, phi_lam, epochs, trainDataLoader, testDataLoader, loss_fn, lam_min, lam_max, 
+                        true_losses, lr=1e-3, step_size=None, const=None, weightd_avg=False, SGD=False, init_weight=None, 
                         intercept=True, record_frequency=10, distribution='uniform', device='cpu', trace_frequency=-1):
     # build the model
     model = Basis_TF_SGD(input_dim, basis_dim, phi_lam, init_weight=init_weight, intercept=intercept).to(device)
-    avg_model = Basis_TF_SGD(input_dim, basis_dim, phi_lam, init_weight=init_weight, intercept=intercept).to(device)
     optimizer = torch.optim.SGD(model.parameters(), lr=lr)
     optimizer.zero_grad()
                           
     # initialize weighted averaging sum
-    avg_weight = avg_model.linear.weight.clone().detach()
+    if weighted_avg:
+        avg_model = Basis_TF_SGD(input_dim, basis_dim, phi_lam, init_weight=init_weight, intercept=intercept).to(device)
+        avg_weight = avg_model.linear.weight.clone().detach()
+        sup_err_history_avg = []
                           
     sup_err_history = []
     num_itr_history = []
-    if diminish:
-        # Define the learning rate scheduler
-        scheduler = StepLR(optimizer, step_size=dim_step, gamma=gamma)  # Decrease LR by a factor of gamma every dim_step epochs
 
     for t in range(epochs):
-        if SGD:
+        if step_size is not None:
             # shrink learning rate
             for param_group in optimizer.param_groups:
                 param_group['lr'] = step_size(t, const)
                 
         train_lsp(trainDataLoader, model, loss_fn, optimizer, distribution, device)
 
-        if SGD: 
+        if weightd_avg and t>50: 
             rho = 2 / (t+3)
             avg_weight = (1-rho) * avg_weight + rho * model.linear.weight.clone().detach()
-            
-        if diminish:
-            # Update the learning rate
-            scheduler.step()
+        else:
+            avg_weight = model.linear.weight.clone().detach()
             
         if (t+1) % record_frequency == 0:
-            # print(model.linear.weight.clone().detach())
             num_itr_history.append(t+1)
-            if SGD:
+            if weightd_avg:
                 with torch.no_grad():
                     avg_model.linear.weight.copy_(avg_weight)
                 sup_err = get_sup_error_lsp(lam_min, lam_max, true_losses,
@@ -54,5 +50,8 @@ def learn_solution_path(input_dim, basis_dim, phi_lam, epochs, trainDataLoader, 
             if (trace_frequency > 0) & ((t+1) % trace_frequency == 0):
                 print(f"--------approximate solution path for # itr = {t+1} complete--------")
                 print(f"# itr: {t+1}\t sup error: {sup_err}")
-
-    return num_itr_history, sup_err_history, model, avg_model
+    if weighted_avg:
+        return num_itr_history, sup_err_history, model.linear.weight.clone().detach()
+    else:
+        return num_itr_history, sup_err_history, avg_weight
+    
