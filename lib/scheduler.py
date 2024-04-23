04e2aa096e65e5c2ec1f316e161455fcd2e23b53
Cumberkid <40275622+Cumberkid@users.noreply.github.com>
2024-04-23 15:40:47 +0800

    Update scheduler.py

--- lib/scheduler.py
+++ lib/scheduler.py
@@ -1,6 +1,6 @@
-import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
 
-class AlphaT_MinLR_Scheduler(optim.lr_scheduler._LRScheduler):
+class AlphaT_MinLR_Scheduler(lr_scheduler._LRScheduler):
     def __init__(self, optimizer, init_lr, alpha):
         self.init_lr = init_lr
         self.alpha = alpha
@@ -8,3 +8,12 @@ class AlphaT_MinLR_Scheduler(optim.lr_scheduler._LRScheduler):
 
     def get_lr(self):
         return [min(self.init_lr, self.alpha / (self.last_epoch + 1)) for _ in self.optimizer.param_groups]
+
+
+class CustomScheduler(lr_scheduler._LRScheduler):
+    def __init__(self, optimizer, gamma=0.97, last_epoch=-1):
+        self.factor = gamma
+        super(CustomScheduler, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        return [base_lr * self.factor for base_lr in self.base_lrs]
