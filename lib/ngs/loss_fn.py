2e4cbe0636520564ff5748ade2e49d07a1b80983
Cumberkid <40275622+Cumberkid@users.noreply.github.com>
2024-05-10 11:17:19 +0800

    Update loss_fn.py

--- lib/ngs/loss_fn.py
+++ lib/ngs/loss_fn.py
@@ -1,10 +1,30 @@
-import torch
-
 def reg_logit(lam, X, y, model):
     pred = model(X)
     loss = (1 - lam) * model.criterion(pred.view(-1, 1), y.view(-1, 1))
-    loss += lam * 0.5 * model.ridge_term()
+    loss = loss/len(X) + lam * 0.5 * model.ridge_term()
     return loss
+
+# def weighted_logit(lam, X, y, model):
+#     X_major = X[y == 1]
+#     y_major = y[y == 1]
+#     X_minor = X[y == 0]
+#     y_minor = y[y == 0]
+
+#     # Compute predicted y_hat
+#     pred_major = model(X_major)
+#     pred_minor = model(X_minor)
+
+#     # reweighted loss function
+#     loss = (1 - lam) * model.criterion(pred_major.view(-1, 1), y_major.view(-1, 1))
+#     loss += lam * model.criterion(pred_minor.view(-1, 1), y_minor.view(-1, 1))
+
+#     return loss
+
+# def reg_weighted_logit(lam, X, y, model):
+#     loss = weighted_logit(lam, X, y, model)
+#     loss += 0.25 * 0.5 * model.ridge_term()
+
+#     return loss
 
 def weighted_logit(lam, X, y, model):
     X_major = X[y == 1]
@@ -16,37 +36,15 @@ def weighted_logit(lam, X, y, model):
     pred_major = model(X_major)
     pred_minor = model(X_minor)
 
-    # reweighted loss function
-    loss = (1 - lam) * model.criterion(pred_major.view(-1, 1), y_major.view(-1, 1))
-    loss += lam * model.criterion(pred_minor.view(-1, 1), y_minor.view(-1, 1))
+    # reweighted loss function with bias correction due to mini-batching
+    loss = (1 - lam) * (len(X)/len(X_major)) * model.criterion(pred_major.view(-1, 1), y_major.view(-1, 1))
+    loss += lam * (len(X)/len(X_minor)) * model.criterion(pred_minor.view(-1, 1), y_minor.view(-1, 1))
+    loss = loss/len(X)
 
     return loss
 
 def reg_weighted_logit(lam, X, y, model):
     loss = weighted_logit(lam, X, y, model)
-    loss += 0.25 * 0.5 * model.ridge_term()
-
-    return loss
-
-def weighted_logit_sgd(lam, X, y, model):
-    X_major = X[y == 1]
-    y_major = y[y == 1]
-    X_minor = X[y == 0]
-    y_minor = y[y == 0]
-
-    # Compute predicted y_hat
-    pred_major = model(X_major)
-    pred_minor = model(X_minor)
-
-    criterion = torch.nn.BCEWithLogitsLoss(reduction='sum')
-    # reweighted loss function with bias correction due to mini-batching
-    loss = (1 - lam) * (len(X)/len(X_major)) * criterion(pred_major.view(-1, 1), y_major.view(-1, 1))
-    loss += lam * (len(X)/len(X_minor)) * criterion(pred_minor.view(-1, 1), y_minor.view(-1, 1))
-
-    return loss/len(X)
-
-def reg_weighted_logit_sgd(lam, X, y, model):
-    loss = weighted_logit_sgd(lam, X, y, model)
     loss += 0.25 * 0.5 * model.ridge_term()
 
     return loss
