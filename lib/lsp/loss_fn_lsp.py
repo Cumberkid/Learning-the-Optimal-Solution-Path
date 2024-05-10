9744bcd189534f1695e3a1bfa43585408a7adc8d
Cumberkid <40275622+Cumberkid@users.noreply.github.com>
2024-05-10 11:15:09 +0800

    Update loss_fn_lsp.py

--- lib/lsp/loss_fn_lsp.py
+++ lib/lsp/loss_fn_lsp.py
@@ -1,7 +1,5 @@
 import torch
 
-criterion = torch.nn.BCEWithLogitsLoss()
-
 def reg_logit(lam, X, y, model, device="cpu"):
     # Compute predicted y_hat
     theta = model(lam, device)
@@ -13,7 +11,7 @@ def reg_logit(lam, X, y, model, device="cpu"):
     else:
         pred = torch.mm(X, theta.view(-1, 1))
     # print(theta[0])
-
+    criterion = torch.nn.BCEWithLogitsLoss(reduction="mean")
     loss = (1 - lam) * criterion(pred.view(-1, 1), y.view(-1, 1))
     loss += lam * 0.5 * theta.norm(p=2) ** 2
 
@@ -39,11 +37,41 @@ def weighted_logit(lam, X, y, model, device="cpu"):
     else:
         pred_major = torch.mm(X_major, theta.view(-1, 1))
         pred_minor = torch.mm(X_minor, theta.view(-1, 1))
-    # fair loss function
-    loss = (1 - lam) * criterion(pred_major.view(-1, 1), y_major.view(-1, 1))
-    loss += lam * criterion(pred_minor.view(-1, 1), y_minor.view(-1, 1))
+    # reweighted loss function
+    criterion = torch.nn.BCEWithLogitsLoss(reduction="sum")
+    loss = (1 - lam) * (len(X)/len(X_major)) * criterion(pred_major.view(-1, 1), y_major.view(-1, 1))
+    loss += lam * (len(X)/len(X_minor)) * criterion(pred_minor.view(-1, 1), y_minor.view(-1, 1))
+    loss = loss/len(X)
 
     return loss
+
+# def reg_weighted_logit(lam, X, y, model, device="cpu"):
+#     X_major = X[y == 1]
+#     y_major = y[y == 1]
+#     X_minor = X[y == 0]
+#     y_minor = y[y == 0]
+
+#     # compute predicted y_hat
+#     theta = model(lam, device)
+#     # print(theta[0])
+    
+#     if model.intercept:
+#         pred_major = torch.mm(X_major, theta[1:].view(-1, 1))
+#         pred_minor = torch.mm(X_minor, theta[1:].view(-1, 1))
+#         const_major = torch.ones(len(X_major), 1).to(device)
+#         pred_major += torch.mm(const_major, theta[0].view(-1, 1))
+#         const_minor = torch.ones(len(X_minor), 1).to(device)
+#         pred_minor += torch.mm(const_minor, theta[0].view(-1, 1))
+#     else:
+#         pred_major = torch.mm(X_major, theta.view(-1, 1))
+#         pred_minor = torch.mm(X_minor, theta.view(-1, 1))
+#     # reweighted loss function
+#     criterion = torch.nn.BCEWithLogitsLoss(reduction="mean")
+#     loss = (1 - lam) * criterion(pred_major.view(-1, 1), y_major.view(-1, 1))
+#     loss += lam * criterion(pred_minor.view(-1, 1), y_minor.view(-1, 1))
+#     loss += 0.25 * 0.5 * theta.norm(p=2) ** 2
+
+#     return loss
 
 def reg_weighted_logit(lam, X, y, model, device="cpu"):
     X_major = X[y == 1]
@@ -65,9 +93,11 @@ def reg_weighted_logit(lam, X, y, model, device="cpu"):
     else:
         pred_major = torch.mm(X_major, theta.view(-1, 1))
         pred_minor = torch.mm(X_minor, theta.view(-1, 1))
-    # fair loss function
-    loss = (1 - lam) * criterion(pred_major.view(-1, 1), y_major.view(-1, 1))
-    loss += lam * criterion(pred_minor.view(-1, 1), y_minor.view(-1, 1))
+    # reweighted loss function with bias correction due to mini-batching
+    criterion = torch.nn.BCEWithLogitsLoss(reduction="sum")
+    loss = (1 - lam) * (len(X)/len(X_major)) * criterion(pred_major.view(-1, 1), y_major.view(-1, 1))
+    loss += lam * (len(X)/len(X_minor)) * criterion(pred_minor.view(-1, 1), y_minor.view(-1, 1))
+    loss = loss/len(X)
     loss += 0.25 * 0.5 * theta.norm(p=2) ** 2
 
     return loss
