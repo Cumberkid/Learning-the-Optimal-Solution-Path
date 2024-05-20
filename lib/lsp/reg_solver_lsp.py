1291280dbf4e16f6fd47dda86ee15d1e1d1034fa
Cumberkid <40275622+Cumberkid@users.noreply.github.com>
2024-05-20 23:12:01 +0800

    Update reg_solver_lsp.py

--- lib/lsp/reg_solver_lsp.py
+++ lib/lsp/reg_solver_lsp.py
@@ -4,9 +4,11 @@ import torch
 # avg_weight: keeps track of and updates weighted average iterates including before current epoch according to Lacoste-Julien et al.
 # step_size function take in 2 parameters: current iteration number, and a self-defined constant const
 # step_size function returns the learning rate for current iteration
-def train_lsp(itr, avg_weight, dataloader, model, loss_fn, optimizer, step_size=None, const=None, distribution='uniform', device='cpu'):
+def train_lsp(itr, init_weight, dataloader, model, loss_fn, optimizer, weighted_avg=True, 
+              step_size=None, const=None, distribution='uniform', device='cpu'):
     model.train()
-    avg_weight = avg_weight
+    if weighted_avg:
+        avg_weight = init_weight
     for batch, (X_train, y_train) in enumerate(dataloader):
         X_train, y_train = X_train.to(device), y_train.to(device)
 
@@ -31,16 +33,19 @@ def train_lsp(itr, avg_weight, dataloader, model, loss_fn, optimizer, step_size=
         # record raw gradient
         grad = model.linear.weight.grad.clone().detach()
         optimizer.step()
-
-        # update weighted average iterates
-        rho = 2 / (itr+3)
-        itr += 1
-        if itr>50: # forgets first 50 iterations which are usually bad
-            avg_weight = (1-rho) * avg_weight + rho * model.linear.weight.clone().detach()
-        else:
-            avg_weight = model.linear.weight.clone().detach()
-
-    return grad, avg_weight, itr
+        
+        if weighted_avg:
+            # update weighted average iterates
+            rho = 2 / (itr+3)
+            itr += 1
+            if itr > 50: # forgets first 50 iterations which are usually bad
+                avg_weight = (1-rho) * avg_weight + rho * model.linear.weight.clone().detach()
+            else:
+                avg_weight = model.linear.weight.clone().detach()
+    if weighted_avg:
+        return grad, avg_weight, itr
+    else:
+        return grad, model.linear.weight.clone().detach(), itr
     
 # Test function computes loss for a fixed input hyperparameter lam
 def test_lsp(dataloader, model, loss_fn, lam, device='cpu'):
