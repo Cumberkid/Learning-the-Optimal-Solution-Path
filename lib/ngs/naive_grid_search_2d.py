3997ac869acb4a66dc2e8a38ee2b98297b54deaa
Cumberkid <40275622+Cumberkid@users.noreply.github.com>
2024-10-16 17:57:11 -0700

    Add files via upload

--- lib/ngs/naive_grid_search_2d.py
+++ lib/ngs/naive_grid_search_2d.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-from lib.ngs.solver import train, test
+from lib.ngs.solver import train_bfgs, test
 from lib.ngs.portfolio_alloc_module import Portfolio_Allocation
 
 """# Naive Grid Search"""
@@ -29,12 +29,12 @@ def GD_on_a_grid(fix_lam, running_lam, lam_max, epochs, loss_fn, model, test_mod
 
     early_stop = False
     itr = 0
-    passes = 0
+    grad_calls = 0
     error = 0
                    
     for t in range(epochs):
 
-        itr, weight, intercept = train(itr, weight, intercept, trainDataLoader, model, loss_fn, 
+        itr, weight, intercept = train_bfgs(itr, weight, intercept, trainDataLoader, model, loss_fn, 
                                        optimizer, weighted_avg, step_size, const, device)
         # print(model.hyper_param, weight)
         if true_loss_list is not None:
@@ -52,7 +52,7 @@ def GD_on_a_grid(fix_lam, running_lam, lam_max, epochs, loss_fn, model, test_mod
                 # check if we are within stopping criterion
                 if oracle:
                   if error <= stopping_criterion:
-                      passes += (t+1)
+                      grad_calls += (t+1)
                       early_stop = True
                       break  # Early stop
         # if change in loss < 1e-7:
@@ -61,9 +61,9 @@ def GD_on_a_grid(fix_lam, running_lam, lam_max, epochs, loss_fn, model, test_mod
         #     break  # Early stop
 
     if not early_stop:
-        passes += epochs
+        grad_calls += epochs
         
-    return passes, error, weight, intercept
+    return itr, grad_calls, error, weight, intercept
 
 """Naive Grid Search starts from $\lambda = 1$ and decreases $\lambda$ by $\Delta\lambda = \frac{\lambda_\text{max} - \lambda_\text{min}}{\text{# of grid}}$. The model trained on each grid point $(\lambda - \Delta\lambda)$ initializes weight with the linear weight of the model trained on the previous grid point $\lambda$."""
 
@@ -81,7 +81,8 @@ def naive_grid_search(fix_lam, lam_min, lam_max, num_grid, epochs, loss_fn, trai
     hyper_params = []
     weights = []
     intercepts = []
-    total_passes = 0
+    total_evals = 0
+    total_grad_calls = 0
     grid_pass_error = 0
     # create a list of lambda's
     lambdas = np.linspace(lam_max, lam_min, num_grid)
@@ -91,12 +92,12 @@ def naive_grid_search(fix_lam, lam_min, lam_max, num_grid, epochs, loss_fn, trai
     intercept = 0
     model = Portfolio_Allocation(data_input_dim, 1, [fix_lam, lam_max], weight).to(device)
     test_model = Portfolio_Allocation(data_input_dim, 1, [fix_lam, lam_max], weight).to(device)
-    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    optimizer = torch.optim.LBFGS(model.parameters(), lr=lr, max_iter=10)
     optimizer.zero_grad()
     
     for running_lam in lambdas:
         # print(f"Running model on lambda = {running_lam}")
-        passes, grid_error, weight, intercept = GD_on_a_grid(fix_lam, running_lam, lam_max, epochs, 
+        evals, grad_calls, grid_error, weight, intercept = GD_on_a_grid(fix_lam, running_lam, lam_max, epochs, 
                                                             loss_fn, model, test_model, optimizer,
                                                             trainDataLoader=trainDataLoader,
                                                             step_size=step_size, const=const, 
@@ -113,10 +114,11 @@ def naive_grid_search(fix_lam, lam_min, lam_max, num_grid, epochs, loss_fn, trai
         intercepts.append(intercept)               
 
         hyper_params.append(model.hyper_param)
-        total_passes += passes
+        total_evals += evals
+        total_grad_calls += grad_calls
         grid_pass_error = max([grid_pass_error, grid_error])
 
-    return total_passes, hyper_params, intercepts, weights, grid_pass_error
+    return total_evals, total_grad_calls, hyper_params, intercepts, weights, grid_pass_error
 
 
 
@@ -125,7 +127,8 @@ def naive_grid_search_2d(lam_min_2d, lam_max_2d, num_grid_2d, epochs, loss_fn, t
                       testDataLoader=None, oracle=False, true_loss_list=None, stopping_criterion=None, 
                       check_frequency=5, device="cpu"):
     
-    total_passes_2d = 0
+    total_evals_2d = 0
+    total_grad_calls_2d = 0
     hyper_params_2d = []
     weights_2d = []
     intercepts_2d = []
@@ -146,7 +149,7 @@ def naive_grid_search_2d(lam_min_2d, lam_max_2d, num_grid_2d, epochs, loss_fn, t
             # print(f"nearest i = {i}\t lam = {lam}")
             
 
-        total_passes, hyper_params, intercepts, weights, grid_pass_error = naive_grid_search(fix_lam, lam_min_2d[1], lam_max_2d[1], num_grid_2d[1], 
+        total_evals, total_grad_calls, hyper_params, intercepts, weights, grid_pass_error = naive_grid_search(fix_lam, lam_min_2d[1], lam_max_2d[1], num_grid_2d[1], 
                                                                                              epochs, loss_fn, trainDataLoader,
                                                                                              data_input_dim, lr=lr, step_size=step_size, 
                                                                                              const=const, weighted_avg=weighted_avg, 
@@ -155,10 +158,11 @@ def naive_grid_search_2d(lam_min_2d, lam_max_2d, num_grid_2d, epochs, loss_fn, t
                                                                                              stopping_criterion=stopping_criterion, 
                                                                                              check_frequency=check_frequency, device=device)
         
-        total_passes_2d += total_passes
+        total_evals_2d += total_evals
+        total_grad_calls_2d += total_grad_calls
         hyper_params_2d.append(hyper_params)
         weights_2d.append(weights)
         intercepts_2d.append(intercepts)
         grid_pass_error_2d = max([grid_pass_error, grid_pass_error_2d])
 
-    return total_passes_2d, hyper_params_2d, intercepts_2d, weights_2d, grid_pass_error_2d
+    return total_evals_2d, total_grad_calls_2d, hyper_params_2d, intercepts_2d, weights_2d, grid_pass_error_2d
--- lib/ngs/solver.py
+++ lib/ngs/solver.py
@@ -38,6 +38,49 @@ def train(itr, init_weight, init_intercept, dataloader, model, loss_fn, optimize
                 intercept = model.linear.bias.clone().detach().squeeze()
     return itr, weight, intercept
 
+# trace_frequency is measured in number of batches. -1 means don't print
+def train_bfgs(itr, init_weight, init_intercept, dataloader, model, loss_fn, optimizer, weighted_avg=False, step_size=None, const=None, device='cpu'):
+    # size = len(dataloader.dataset)
+    model.train()
+    weight = init_weight #initialize
+    intercept = init_intercept 
+    # here, the "batch" notion takes care of randomization
+    for batch, (X_train, y_train) in enumerate(dataloader):
+        X_train, y_train = X_train.to(device), y_train.to(device)
+        # print(batch, len(X_train))
+        function_evaluations=0
+        def closure():
+            nonlocal function_evaluations  # Use the nonlocal keyword to modify the counter
+            function_evaluations += 1
+            loss = loss_fn(model.hyper_param, X_train, y_train, model, device)
+            # print(loss)
+        
+            # if step_size is not None:
+            #     # shrink learning rate as customized
+            #     lr = step_size(itr, const)
+            #     for param_group in optimizer.param_groups:
+            #         param_group['lr'] = lr
+            # Backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            return loss
+        
+        optimizer.step(closure)
+
+        # update weighted average iterate
+        # rho = 2 / (itr+3)
+        itr += function_evaluations
+        # if weighted_avg and itr > 50:
+        #     weight = (1-rho) * weight + rho * model.linear.weight.clone().detach().squeeze()
+        #     if model.linear.bias is not None:
+        #         intercept = (1-rho) * intercept + rho * model.linear.bias.clone().detach().squeeze()
+
+        # else:
+        weight = model.linear.weight.clone().detach().squeeze()
+        if model.linear.bias is not None:
+            intercept = model.linear.bias.clone().detach().squeeze()
+    return itr, weight, intercept
+
 # test function computes objective loss for a specific input hyperparameter lam
 def test(dataloader, model, loss_fn, hyper_params, device='cpu'):
     model.eval() # important
